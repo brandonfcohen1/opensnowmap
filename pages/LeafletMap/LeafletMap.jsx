eae70db0612fe79d4d2619bbf447ee8c339e6559
Brandon Cohen <brandon.f.cohen@gmail.com>
2021-11-27 22:50:19 -0500

    cleanup

--- pages/LeafletMap/LeafletMap.jsx
+++ pages/LeafletMap/LeafletMap.jsx
@@ -3,19 +3,11 @@ import {
   MapContainer,
   TileLayer,
   LayersControl,
-  useMap,
   useMapEvents,
-  Marker,
   Popup,
 } from "react-leaflet";
 import { DynamicMapLayer } from "react-esri-leaflet";
 import * as esri from "esri-leaflet";
-//import SnowCoverage from "../SnowCoverage/SnowCoverage";
-const icon = L.icon({
-  iconUrl: "/images/marker-icon.png",
-  shadowUrl: "/images/marker-shadow.png",
-  iconAnchor: [14, 34]
-});
 
 const latlngDisp = (ll) => {
   return (
@@ -29,46 +21,44 @@ const latlngDisp = (ll) => {
 
 const HandleClick = () => {
   const [position, setPosition] = useState(null);
+  const [snowDepth, setSnowDepth] = useState("");
   const map = useMapEvents({
     click(e) {
-      //const map = useMap();
       setPosition(e.latlng);
-      // esri
-      //   .identifyFeatures({
-      //     url: "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Observations/NOHRSC_Snow_Analysis/MapServer",
-      //   })
-      //   .layers("all:3,7")
-      //   .on(map)
-      //   .at(e.latlng)
-      //   .run(function (error, featureCollection) {
-      //     if (error) {
-      //       console.log(error);
-      //       return;
-      //     }
-      //     const snowDepth =
-      //       Math.round(
-      //         10 *
-      //           featureCollection.features.filter((p) => p.layerId === 3)[0]
-      //             .properties["Pixel Value"]
-      //       ) / 10;
-      //     const snowDepthPopup = L.popup()
-      //       .setLatLng(e.latlng)
-      //       .setContent(
-      //         "<b>Location:</b> " +
-      //           latlngDisp(e.latlng) +
-      //           "<br><b>Snow depth:</b> " +
-      //           snowDepth +
-      //           " in."
-      //       )
-      //       .addTo(map);
-      //   });
+      setSnowDepth("");
+      esri
+        .identifyFeatures({
+          url: "https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Observations/NOHRSC_Snow_Analysis/MapServer",
+        })
+        .layers("all:3,7")
+        .on(map)
+        .at(e.latlng)
+        .run(function (error, featureCollection) {
+          if (error) {
+            console.log(error);
+            return;
+          }
+          const _snowDepth = Math.max(
+            Math.round(
+              10 *
+                featureCollection.features.filter((p) => p.layerId === 3)[0]
+                  .properties["Pixel Value"]
+            ) / 10,
+            0
+          );
+          setSnowDepth(_snowDepth);
+        });
     },
   });
 
   return position === null ? null : (
-    <Marker position={position} icon={icon}>
-      <Popup>You are here</Popup>
-    </Marker>
+    <Popup position={position}>
+      <b>{"Snow Depth: "}</b>
+      {snowDepth}
+      <br />
+      <b>{"Location: "}</b>
+      {latlngDisp(position)}
+    </Popup>
   );
 };
 
@@ -82,14 +72,12 @@ export default function LeafletMap() {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
         </LayersControl.BaseLayer>
-        <LayersControl.Overlay name="Snow Depth">
+        <LayersControl.Overlay checked name="Snow Depth">
           <DynamicMapLayer
             url="https://idpgis.ncep.noaa.gov/arcgis/rest/services/NWS_Observations/NOHRSC_Snow_Analysis/MapServer"
             opacity={0.5}
             f="image"
           />
-
-          {/* <SnowCoverage /> */}
         </LayersControl.Overlay>
       </LayersControl>
       <HandleClick />
